38cbb1a479e6c865b6baaa3dc0842376082e0c12
xKingPanda <mjamesfu@gmail.com>
2026-05-23 19:05:33 -0500

    Create basic Tkinter app window

---
+++ app.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.title("Password Generator")
+    root.geometry("500x400")
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
